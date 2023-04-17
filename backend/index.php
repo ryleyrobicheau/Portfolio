16b5a2e977bd72cea926d30754216c776e043bc2
ryleyrobicheau <ryleyrobicheau@gmail.com>
2023-04-17 10:36:27 -0700

    moved files locally into github repo

---
+++ backend/index.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form data
+  $firstName = $_POST["first-name"];
+  $lastName = $_POST["last-name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  // Set the email recipient, subject, and message body
+  $to = "your-email@example.com";
+  $subject = "New contact form submission from $firstName $lastName";
+  $body = "First Name: $firstName\nLast Name: $lastName\nEmail: $email\nMessage: $message";
+
+  // Send the email
+  mail($to, $subject, $body);
+
+  // Redirect the user to a confirmation page
+  header("Location: thank-you.html");
+  exit();
+}
+?>
